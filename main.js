872acecae1bff27a2bff055ed479dcc58b2f0317
Luis123 <84759536+Luis123-hehe@users.noreply.github.com>
2022-02-22 15:23:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+status = "";
+   
+function setup() {
+    canvas = createCanvas(480, 420);
+    canvas.center();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380)
+}
